60cdc732828a6f7c14d21a0ead4dd05831512f03
Olaobaju Abraham <39011309+bajoski34@users.noreply.github.com>
2020-06-25 18:15:32 +0100

    Update rave-standard.php

--- rave-standard.php
+++ rave-standard.php
@@ -1,29 +1,5 @@
 <?php
 //data required. You can check the doc for more options to add....
-{
-       "tx_ref": "hooli-tx-1920bbtytty",
-        "amount": "100",
-        "currency": "NGN",
-        "redirect_url": "https://webhook.site/9d0b00ba-9a69-44fa-a43d-a82c33c36fdc",
-        "payment_options":"card",
-        "meta": {
-          "consumer_id": 23,
-          "consumer_mac": "92a3-912ba-1192a"
-        },
-        "customer": {
-         "email": "user@gmail.com",
-          "phonenumber": "080****4528",
-          "name":"Yemi Desola"
-        },
-       "customizations": {
-          "title": "Pied Piper Payments",
-          "description": "Middleout isn't free. Pay the price",
-          "logo": "https://assets.piedpiper.com/logo.png"
-            }
-}
-
-
-
 $postdata = array(
         "tx_ref"=>uniqid().uniqid(),
         "amount": "100",
